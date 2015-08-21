bf1094bbacce3c01d31a0e5370cd20327aaaa8f9
Michael Bunker <student@epicodus.com>
2015-08-21 10:26:56 -0700

    add #save spec for Stylists class

--- spec/stylists_spec.rb
+++ spec/stylists_spec.rb
@@ -8,5 +8,12 @@ describe Stylists do
     end
   end
 
+  describe '#save' do
+    it 'saves a stylist to the DB' do
+      stylist = Stylists.new({name: 'Bob Cousy'})
+      stylist.save
+      expect(Stylists.all).to eq [stylist]
+    end
+  end
 
 end
