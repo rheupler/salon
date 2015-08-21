b8471ce977d7fb419312054b84557b3c253fa6a0
Michael Bunker <student@epicodus.com>
2015-08-21 10:22:37 -0700

    add .all method to Stylists class

--- lib/stylists.rb
+++ lib/stylists.rb
@@ -6,5 +6,16 @@ class Stylists
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id, nil)
   end
-  
+
+  def self.all
+    returned_stylists = DB.exec('SELECT * FROM stylists;')
+    stylists = []
+    returned_stylists.each do |stylist|
+      name = stylist.fetch('name')
+      id = stylist.fetch('id').to_i
+      stylists.push(Clients.new({name: name, id: id}))
+    end
+    stylists
+  end
+
 end
