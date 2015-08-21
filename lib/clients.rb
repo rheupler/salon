02a1859c59594d68617d440eba6bd127266902c0
Michael Bunker <student@epicodus.com>
2015-08-21 10:00:03 -0700

    add .all method to Clients class

--- lib/clients.rb
+++ lib/clients.rb
@@ -7,4 +7,15 @@ class Clients
     @id = attributes.fetch(:id, nil)
   end
 
+  def self.all
+    returned_clients = DB.exec('SELECT * FROM clients;')
+    clients = []
+    returned_clients.each do |client|
+      name = client.fetch('name')
+      id = client.fetch('id').to_i
+      cleints.push(Clients.new({name: name, id: id}))
+    end
+    clients
+  end
+
 end
